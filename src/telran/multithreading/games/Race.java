ccf2b41ca15cf20ef9b486f5ac0ba263adf19482
StasSaltykov <stasyanych92@gmail.com>
2022-11-19 15:13:03 +0200

    after CW#41

--- src/telran/multithreading/games/Race.java
+++ src/telran/multithreading/games/Race.java
@@ -5,27 +5,33 @@ public class Race {
 	private int minSleep;
 	private int maxSleep;
 	private int winner = -1;
+
 	public Race(int distance, int minSleep, int maxSleep) {
 		this.distance = distance;
 		this.minSleep = minSleep;
 		this.maxSleep = maxSleep;
 	}
+
 	public int getWinner() {
 		return winner;
 	}
+
 	public void setWinner(int winner) {
 		if (this.winner == -1) {
 			this.winner = winner;
 		}
 	}
+
 	public int getDistance() {
 		return distance;
 	}
+
 	public int getMinSleep() {
 		return minSleep;
 	}
+
 	public int getMaxSleep() {
 		return maxSleep;
 	}
-	
+
 }
--- src/telran/multithreading/games/Runner.java
+++ src/telran/multithreading/games/Runner.java
@@ -1,25 +1,27 @@
 package telran.multithreading.games;
 
 public class Runner extends Thread {
-private Race race;
-private int runnerId;
-public Runner(Race race, int runnerId) {
-	this.race = race;
-	this.runnerId = runnerId;
-}
-@Override
-public void run() {
-	int sleepRange = race.getMaxSleep() - race.getMinSleep() + 1;
-	int minSleep = race.getMinSleep();
-	int distance = race.getDistance();
-	for (int i = 0; i < distance; i++) {
-		try {
-			sleep((long) (minSleep + Math.random() * sleepRange));
-		} catch (InterruptedException e) {
-			throw new IllegalStateException();
-		}
-		System.out.println(runnerId);
+	private Race race;
+	private int runnerId;
+
+	public Runner(Race race, int runnerId) {
+		this.race = race;
+		this.runnerId = runnerId;
 	}
-	race.setWinner(runnerId);
-}
+
+	@Override
+	public void run() {
+		int sleepRange = race.getMaxSleep() - race.getMinSleep() + 1;
+		int minSleep = race.getMinSleep();
+		int distance = race.getDistance();
+		for (int i = 0; i < distance; i++) {
+			try {
+				sleep((long) (minSleep + Math.random() * sleepRange));
+			} catch (InterruptedException e) {
+				throw new IllegalStateException();
+			}
+			System.out.println(runnerId);
+		}
+		race.setWinner(runnerId);
+	}
 }
