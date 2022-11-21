a1443f271289331d317c24ce3b08b7dfd5404463
StasSaltykov <stasyanych92@gmail.com>
2022-11-21 20:19:57 +0200

    CW #42

--- src/telran/multithreading/Truck.java
+++ src/telran/multithreading/Truck.java
@@ -1,11 +1,12 @@
 package telran.multithreading;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class Truck extends Thread {
 	private int load;
 	private int nLoads;
-	private static long elevator1;
-	private static long elevator2;
-	private static final Object mutex = new Object();
+	private static AtomicLong elevator1 = new AtomicLong();
+	private static AtomicLong elevator2 = new AtomicLong();
 
 	public Truck(int load, int nLoads) {
 		this.load = load;
@@ -20,23 +21,21 @@ public class Truck extends Thread {
 		}
 	}
 
-	synchronized static private void loadElevator2(int load) {
-		{
-			elevator2 += load;
-		}
+	static private void loadElevator2(int load) {
+		elevator2.addAndGet(load);
 
 	}
 
-	synchronized static private void loadElevator1(int load) {
-		elevator1 += load;
+	static private void loadElevator1(int load) {
+		elevator1.addAndGet(load);
 
 	}
 
 	public static long getElevator1() {
-		return elevator1;
+		return elevator1.get();
 	}
 
 	public static long getElevator2() {
-		return elevator2;
+		return elevator2.get();
 	}
 }
