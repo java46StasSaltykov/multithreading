efa491a78d27ff98277b2da38b58d0e6d030a9d7
StasSaltykov <stasyanych92@gmail.com>
2022-11-23 00:01:22 +0200

    HW #42

--- src/telran/multithreading/games/Race.java
+++ src/telran/multithreading/games/Race.java
@@ -1,10 +1,12 @@
 package telran.multithreading.games;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Race {
 	private int distance;
 	private int minSleep;
 	private int maxSleep;
-	private int winner = -1;
+	private AtomicInteger winner = new AtomicInteger();
 
 	public Race(int distance, int minSleep, int maxSleep) {
 		this.distance = distance;
@@ -13,12 +15,12 @@ public class Race {
 	}
 
 	public int getWinner() {
-		return winner;
+		return winner.get();
 	}
 
 	public void setWinner(int winner) {
-		if (this.winner == -1) {
-			this.winner = winner;
+		if (this.winner.get() == 0) {
+			this.winner.addAndGet(winner);
 		}
 	}
 
