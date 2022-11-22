c02f5e164d4ba9a10ad6f376f50b08af07adadf7
StasSaltykov <stasyanych92@gmail.com>
2022-11-22 22:41:52 +0200

    CW #42

---
+++ src/telran/multithreading/Worker.java
@@ -0,0 +1,47 @@
+package telran.multithreading;
+
+public class Worker extends Thread {
+	
+	static Object resource1 = new Object();
+	static Object resource2 = new Object();
+	static Object resource3 = new Object();
+	static int nRuns = 1;
+	
+	void f1() {
+		synchronized (resource1) {
+			synchronized (resource2) {
+				synchronized (resource3) {
+					
+				}
+			}
+		}
+	}
+	
+	void f2() {
+		synchronized (resource1) {
+			synchronized (resource3) {
+
+			}
+		}
+	}
+	
+	void f3() {
+		synchronized (resource2) {
+			synchronized (resource3) {
+				
+			}
+		}
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i < nRuns; i++) {
+			f1();
+			f2();
+			f3();
+		}
+	}
+	
+	
+
+}
