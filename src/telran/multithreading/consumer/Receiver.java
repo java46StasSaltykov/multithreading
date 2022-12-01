06a4bbef963514b917401051d650ece70060cb9e
StasSaltykov <stasyanych92@gmail.com>
2022-12-01 19:57:07 +0200

    HW #44 fixing bug of message lost

--- src/telran/multithreading/consumer/Receiver.java
+++ src/telran/multithreading/consumer/Receiver.java
@@ -1,27 +1,47 @@
 package telran.multithreading.consumer;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import telran.multithreading.MessageBox;
 
 public class Receiver extends Thread {
 	
+	private static AtomicInteger messagesCounter = new AtomicInteger(0);
 	private MessageBox messageBox;
-		
+	private boolean running = true;
+
 	public Receiver(MessageBox messageBox) {
-		//FIXME - thread should not be daemon
-		setDaemon(true);
+
 		this.messageBox = messageBox;
 	}
 
 	@Override
 	public void run() {
-		while (true) {
+		String message = null;
+		while (running) {
 			try {
-				String message = messageBox.get();
-				System.out.println(message + getName());
+				message = messageBox.get();
+				messageProcessing(message);
 			} catch (InterruptedException e) {
 
 			}
 		}
+		while ((message = messageBox.take()) != null) {
+			messageProcessing(message);
+		}
+	}
+
+	private void messageProcessing(String message) {
+		messagesCounter.incrementAndGet();
+		System.out.println(message + getName());
+	}
+
+	public static int getMessagesCounter() {
+		return messagesCounter.get();
+	}
+
+	public void setRunning(boolean running) {
+		this.running = running;
 	}
 
 }
